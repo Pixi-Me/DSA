a812aa0aea009543b5f5fe279a18ed93a9d77f7a
Pixi-Me <ezioboii@gmail.com>
2026-05-15 22:12:05 +0530

    Basics done

--- 1 Basics of Cpp/1_ifElse.cpp
+++ 1 Basics of Cpp/1_ifElse.cpp
@@ -4,13 +4,55 @@ using namespace std;
 int main(){
 
     // IF ADULT
-    int age;
+
+    /*int age;
     cin >> age;
     if (age<18){
         cout << "Nope" <<"\n";
     }
     else {
         cout <<"Welcome Adult";
+    }*/
+
+    //Grade calculator
+
+    /*int marks;
+    cout << "Enter marks:";
+    cin >> marks;
+    if(marks >= 85){
+        cout << 'A';
+    }
+    else if(marks >=75){
+        cout << "AB";
+    }
+    else if(marks>=65){
+        cout << "B";
+    }
+    else if(marks>=55){
+        cout << "D";
+    }
+    else {
+        cout << "Fail";
+    }*/
+
+    // NESTED If
+
+    int age;
+    cout << "Enter your age:";
+    cin >> age;
+    if (age <18){
+        cout << "Not eligible";
+    }
+    else {
+        if(age <=55){
+            cout << "Eligible";
+        }
+        else if (age <=57){
+            cout << "Eligible, retirment soon";
+        }
+        else {
+            cout << "Retirment";
+        }
     }
     return 0;
 }
