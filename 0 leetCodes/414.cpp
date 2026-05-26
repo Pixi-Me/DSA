7ca75dc98214d79ffd53f91d00beb6145a320451
Pixi-Me <ezioboii@gmail.com>
2026-05-26 22:44:34 +0530

    Finally done

--- 0 leetCodes/414.cpp
+++ 0 leetCodes/414.cpp
@@ -4,32 +4,68 @@ using namespace std;
 int thirdLargest(vector<int>& nums){
     int max=nums[0];
     int max2;
+    bool isMax2 = false;
     int max3;
+    bool isMax3 = false;
     for(int i=1;i<nums.size();i++){
-        if(nums[i]==max) continue;
-        
-        else if(nums[i]>max){
-            max3=max2;
-            max2=max;
-            max=nums[i];
+        if (max==nums[i]) continue;
+        else if(isMax2 == false && isMax3==false){
+            if(nums[i]>max){
+                max2=max;
+                isMax2=true;
+                max = nums[i];
             }
-        else if(nums[i]==max2) continue;
-        else if(nums[i]>max2){
-            max3=max2;
-            max2=nums[i];
+            else{
+                max2=nums[i];
+                isMax2=true;
+            }
         }
-        else if(nums[i]==max3) continue;
-        else if(nums[i]>max3){
-            max3=nums[i];
+        else if(isMax3==false){
+            
+            if (nums[i]==max2||nums[i]==max) continue;
+            else if(nums[i]>max){
+                max3=max2;
+                isMax3=true;
+                max2=max;
+                max = nums[i];
+                
+            }
+            else if(nums[i]>max2){
+                max3=max2;
+                isMax3=true;
+                max2=nums[i];
+            }
+            else{
+                max3=nums[i];
+                isMax3=true;
+            }
         }
+        else{
+            if (nums[i]==max3 || nums[i]==max2 || nums[i]==max) continue;
+            else if(nums[i]>max){
+                max3=max2;
+                max2=max;
+                max = nums[i];
+            }
+            else if(nums[i]>max2){
+                max3=max2;
+                max2=nums[i];
+            }
+            else if(nums[i]>max3){
+                max3=nums[i];
+                isMax3=true;
+            }
+        }
+        
+
     }
-    if(!max3 || nums.size()<=2) return *max_element(nums.begin(),nums.end());
-    return max3;
+    if(isMax3==false) return max;
+        return max3;
 }
 int main(){
-    vector<int> nums1 = {1,2,3};
+    vector<int> nums1 = {3,3,3,3,4,3,2,3,3};
     vector<int> nums2 = {2,1,-2147483648};
-    vector<int> nums3 = {14};
-    int max3 = thirdLargest(nums3);
+    vector<int> nums3 = {1,2,3,2,3,4};
+    int max3 = thirdLargest(nums1);
     cout<<max3;
 }
--- 0 leetCodes/tempCodeRunnerFile.cpp
+++ 0 leetCodes/tempCodeRunnerFile.cpp
@@ -1,19 +1,53 @@
-if(nums[i]==max || nums[i]==max2) continue;
-        if(nums[i]>max){
-            max3=max2;
-            max2=max;
-            max=nums[i];
+int max=nums[0];
+    int max2;
+    bool isMax2 = false;
+    int max3;
+    bool isMax3 = false;
+    for(int i=1;i<nums.size();i++){
+        if (max==nums[i]) continue;
+        else if(isMax2 == false && isMax3==false){
+            if(nums[i]>max){
+                max2=max;
+                isMax2=true;
+                max = nums[i];
             }
-        else if(nums[i]>max2){
-            max3=max2;
-            max2=nums[i];
         }
-        else if(nums[i]>max3){
-            max3=nums[i];
+        else if(isMax3==false){
+            if (nums[i]==max2) continue;
+            else if(nums[i]>max){
+                max3=max2;
+                isMax3=true;
+                max2=max;
+                max = nums[i];
+            }
+            else if(nums[i]>max2){
+                max3=max2;
+                isMax3=true;
+                max2=nums[i];
+            }
+            else{
+                max3=nums[i];
+                isMax3=true;
+            }
         }
-        else if(nums[i]==max3){
-            return max3;
+        else{
+            if (nums[i]==max3) continue;
+            else if(nums[i]>max){
+                max3=max2;
+                max2=max;
+                max = nums[i];
+            }
+            else if(nums[i]>max2){
+                max3=max2;
+                max2=nums[i];
+            }
+            else if(nums[i]>max3){
+                max3=nums[i];
+                isMax3=true;
+            }
         }
+        
+
     }
-    if(max3==-2147483648) return *max_element(nums.begin(),nums.end());
-    return max3;
+    if(isMax3==false) return max;
+        return max3;
