cde0e5724895eb253b8c771caf86af94161d061d
Pixi-Me <ezioboii@gmail.com>
2026-05-27 12:56:38 +0530

    Ques done

--- 121.cpp
+++ 121.cpp
@@ -2,19 +2,22 @@
 using namespace std;
 
 int maxProfit(vector<int>& prices){
-    int min=prices[0];
-    int minDate=0;
-    int max;
-    bool isMax = false;
-    for(int i=1;i<prices.size();i++){
-        if(prices[i]<min){
-            min=prices[i];
-            minDate=i;
+    int profit=0;
+    int bestBuy=prices[0];
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<bestBuy){
+                bestBuy=prices[i];
+            }
+            if(profit<prices[i]-bestBuy){
+                profit = prices[i]-bestBuy;
+            }
         }
+        return profit;
     }
-}
+    
 
 int main(){
-    vector<int> prices = {1,7,5,4,3,7,9};
-    cout << maxProfit(prices);
+    vector<int> prices1 = {4,3,2,1,0};
+    vector<int> prices2 = {7,1,5,3,6,4};
+    cout << maxProfit(prices2);
 }
