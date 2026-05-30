3bba710046fc350a0d075a2d4b696ee27f6b8207
Pixi-Me <ezioboii@gmail.com>
2026-05-30 18:18:22 +0530

    30th May

---
+++ 4 Basic Maths/basicMaths.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countDigit(int n){
+    int ans=0;
+    while(n>0){
+        n = n/10;
+        ans++;
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<countDigit(n);
+}
