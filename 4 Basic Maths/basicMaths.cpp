c0a52686e75b9bef4a30a231fd0c749511c9fd78
Pixi-Me <ezioboii@gmail.com>
2026-06-03 22:21:01 +0530

    Some more questions

--- 4 Basic Maths/basicMaths.cpp
+++ 4 Basic Maths/basicMaths.cpp
@@ -9,9 +9,43 @@ int countDigit(int n){
     }
     return ans;
 }
+bool isArmstrong(int x){
+    int sum=0;
+    int n = x;
+    while (n>0){
+        sum += pow(n%10,3);
+        n=n/10;
+        cout<<sum<<endl;
+    }
+    if(sum==x) return true;
+    return false;
+}
+
+void printDivisors(int n){
+    // METHOD 1
+    // for(int i=1;i<=n/2;i++){
+    //     if(n%i==0) cout<<i<<" ";
+    // }
+    // cout<< n;
+
+    //METHOD 2
+    for(int i =1;i<=sqrt(36);i++){
+        if(n%i==0){
+            cout<< i << " ";
+            if(i!=n){
+                cout << n/i << " ";
+            }
+        }
+    }
+}
+
+
 
 int main(){
     int n;
     cin>>n;
-    cout<<countDigit(n);
+    // cout<<countDigit(n);
+    // cout<<isArmstrong(n);
+    printDivisors(n);
+
 }
