f137305240c9fa3e7ced54777bcb559bb93e2c95
Pixi-Me <ezioboii@gmail.com>
2026-06-06 19:17:55 +0530

    h

--- 4 Recursion/1-nPrint.cpp
+++ 4 Recursion/1-nPrint.cpp
@@ -6,6 +6,12 @@ void printN(int i,int n){
     cout<<i<<endl;
     printN(i+1,n);
 }
+void printNto1(int i, int n){
+    if(i>n) return;
+    cout<<n<<endl;
+    printNto1(1,n-1);
+}
 int main(){
     printN(1,10);
+    printNto1(1,190);
 }
---
+++ 4 Recursion/sumOfFirstN.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sumOfN(int n, int i=1, int sum=0){
+    if(i>n) return sum;
+    sum += i;
+    i++;
+    return sumOfN(n, i, sum);
+}
+
+int main(){
+    cout<<sumOfN(3);
+}
